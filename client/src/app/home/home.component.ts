5e7e605da278291b62e4ec06485106451011e8c5
vincentyeungg <vincentyeung0809@gmail.com>
2021-06-19 17:58:35 -0400

    added registration and authentication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -15,6 +16,10 @@ export class HomeComponent implements OnInit {
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 
 }
