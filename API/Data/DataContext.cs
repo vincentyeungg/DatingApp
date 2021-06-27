8ff4699d558ea1aafa2ca5e2ec141f0dd8703f0a
vincentyeungg <vincentyeung0809@gmail.com>
2021-06-27 04:15:37 -0400

    adding like feature, configured join table with EF

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,27 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                .HasKey(k => new {k.SourceUserId, k.LikedUserId}); // will form the primary key of the table
+            
+            // define 1 side of the 1 - many relationship
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // define the remaining side of the 1 - many relationship
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
